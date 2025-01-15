1d988f70e46aabdd4ef87391056e761ff56a6fda
SeoYeong <seoyeong6@naver.com>
2025-01-15 12:03:24 +0900

    feat: sign_up_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/screens/sign_up/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -12,7 +13,7 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'TikTok Clone',
       theme: ThemeData(primaryColor: Color(0xFFE9435A)),
-      home: Container(),
+      home: SignUpScreen(),
     );
   }
 }
